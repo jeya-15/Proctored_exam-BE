33b5c9bc29b243b2b5ecaa864b7d67e5ea691874
jeya-15 <athi68423@gmail.com>
2025-05-06 22:48:16 +0530

    files created

--- server.js
+++ server.js
@@ -1,15 +1,14 @@
 const express = require("express");
 require("dotenv").config();
-const db = require("./Database/db.js");
+const db = require("./database/db.js");
 
 const cors = require("cors");
 const app = express();
 
 const PORT = process.env.PORT;
 
-app.get("/", (req, res) => {
-  res.send("Server is running!");
-});
+app.use("/", require("./routes/routes"));
+
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
